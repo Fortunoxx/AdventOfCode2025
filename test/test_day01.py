777b397338fe0051f761aeca4826dcee602fff9b
Fortunoxx <robert_goetz@hotmail.com>
2025-12-01 23:38:57 +0100

    less text

--- test/test_day01.py
+++ test/test_day01.py
@@ -4,12 +4,14 @@ from src.day01 import solve_part1, solve_part2
 
 day = "01"
 
+
 @pytest.mark.parametrize("day", [day])
 def test_part1(day, expected_value=3):
-    testdata = {"key": "sample", "file": f"test/data/day{day}.sample.dat"}
+    testdata = {"file": f"test/data/day{day}.sample.dat"}
     assert solve_part1(testdata) == expected_value
+
 
 @pytest.mark.parametrize("day", [day])
 def test_part2(day, expected_value=6):
-    testdata = {"key": "sample", "file": f"test/data/day{day}.sample.dat"}
+    testdata = {"file": f"test/data/day{day}.sample.dat"}
     assert solve_part2(testdata) == expected_value
