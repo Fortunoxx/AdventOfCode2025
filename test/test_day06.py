c2911f5d80040be9ecd21cd73808b6ed79c75040
Fortunoxx <robert_goetz@hotmail.com>
2025-12-06 21:15:51 +0100

    day06.test

--- test/test_day06.py
+++ test/test_day06.py
@@ -6,12 +6,12 @@ day = "06"
 
 
 @pytest.mark.parametrize("day", [day])
-def test_part1(day, expected_value=3263827):
+def test_part1(day, expected_value=4277556):
     testdata = {"file": f"test/data/day{day}.sample.dat"}
     assert solve_part1(testdata) == expected_value
 
 
 @pytest.mark.parametrize("day", [day])
-def test_part2(day, expected_value=14):
+def test_part2(day, expected_value=4277556):
     testdata = {"file": f"test/data/day{day}.sample.dat"}
     assert solve_part2(testdata) == expected_value
